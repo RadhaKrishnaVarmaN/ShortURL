43a9a09306ecb670457f4483bea6785bde70b807
RadhaKrishnaVarmaN <106908771+RadhaKrishnaVarmaN@users.noreply.github.com>
2022-07-14 15:39:27 +0530

    Added SQL Short URL Generator
    Short URL Generation and Filling

---
+++ file_short_url.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[file_short_url] (
+    [short_url_id]   INT      IDENTITY (1, 1) NOT NULL,
+    [short_url]      CHAR (8) NOT NULL,
+    [file_id]        INT      NULL,
+    [expiry]         DATETIME NULL,
+    [created_stamp]  DATETIME NOT NULL,
+    [modified_stamp] DATETIME NOT NULL,
+    CONSTRAINT [PK_file_short_url] PRIMARY KEY CLUSTERED ([short_url_id] ASC),
+    CONSTRAINT [UQ_file_short_url] UNIQUE NONCLUSTERED ([short_url] ASC)
+);
+GO
