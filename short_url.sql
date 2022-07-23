1460ae40f2bad60e85449a7b2a84a584808b1cb6
RadhaKrishnaVarmaN <106908771+RadhaKrishnaVarmaN@users.noreply.github.com>
2022-07-23 12:53:47 +0530

    updated short url

---
+++ short_url.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[short_url] (
+    [short_url_id]   INT      IDENTITY (1, 1) NOT NULL,
+    [short_url_code] CHAR (8) NOT NULL,
+    [web_url]        INT      NULL,
+    [expiry_stamp]   DATETIME NULL,
+    [created_stamp]  DATETIME NOT NULL,
+    CONSTRAINT [PK_short_url] PRIMARY KEY CLUSTERED ([short_url_id] ASC),
+    CONSTRAINT [UQ_short_url] UNIQUE NONCLUSTERED ([short_url_code] ASC)
+);
+GO
